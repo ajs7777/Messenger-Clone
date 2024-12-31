cacb239a9422b5c84a42cf9f4c8f6cd5ded385f4
ajs7777 <abhisaha955@gmail.com>
2024-12-31 15:23:25 +0530

    Initial Commit

---
+++ Messenger Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Messenger Clone
+//
+//  Created by Abhijit Saha on 31/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Messenger Clone/Messenger_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Messenger_CloneApp.swift
+//  Messenger Clone
+//
+//  Created by Abhijit Saha on 31/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Messenger_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
